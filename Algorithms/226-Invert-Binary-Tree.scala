78662a1a37209e9d993eb9d9753c3b674cbb8c32
Abhinav Patil <Ubadub@users.noreply.github.com>
2018-09-22 20:21:35 -0400

    Create 226-Invert-Binary-Tree.scala

---
+++ Algorithms/226-Invert-Binary-Tree.scala
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var _value: Int) {
+ *   var value: Int = _value
+ *   var left: TreeNode = null
+ *   var right: TreeNode = null
+ * }
+ */
+object Solution {
+    def invertTree(root: TreeNode): TreeNode = {
+        if (root == null) null
+        else {
+            var invertedLeft: TreeNode = invertTree(root.left)
+            var invertedRight: TreeNode = invertTree(root.right)
+            var ret: TreeNode = TreeNode(root.value)
+            ret.left = invertedRight
+            ret.right = invertedLeft
+            
+            ret
+        }
+    }
+}
